22bd9a6ca7c78813fb0607fa6468052047abb201
Shlynz <75536038+shlynz@users.noreply.github.com>
2022-11-07 21:41:19 +0100

    Added constructor functions for the Cells

--- waveFunctionCollapse.js
+++ waveFunctionCollapse.js
@@ -146,3 +146,44 @@ function defineNeighbours(tiles) {
     })
     return tiles;
 }
+
+/**
+ * Required objects for the WFC
+ *
+ * Holds the value, sockets and valid neighbours for a specific tile
+ * @param {*} value Value for this specific tile
+ * @param {Sockets} sockets {@link Sockets} for this specific tile
+ */
+function Cell(value, sockets) {
+    this.value = value;
+    this.sockets = sockets;
+    this.validNeighbours = new ValidNeighbours([],[],[],[]);
+}
+
+/**
+ * Sockets for a 2D tile
+ * @param {*} up A connection at this edge
+ * @param {*} right A connection at this edge
+ * @param {*} down A connection at this edge
+ * @param {*} left A connection at this edge
+ */
+function Sockets(up, right, down, left) {
+    this.up = up;
+    this.right = right;
+    this.down = down;
+    this.left = left;
+}
+
+/**
+ * Valid neighbours for a 2D tile
+ * @param {Array<Cell>} up An array of allowed cells at this edge
+ * @param {Array<Cell>} right An array of allowed cells at this edge
+ * @param {Array<Cell>} down An array of allowed cells at this edge
+ * @param {Array<Cell>} left An array of allowed cells at this edge
+ */
+function ValidNeighbours(up, right, down, left) {
+    this.up = up;
+    this.right = right;
+    this.down = down;
+    this.left = left;
+}
