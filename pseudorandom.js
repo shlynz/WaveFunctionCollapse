1eb148dd54429eaf73b3c9fcfa3a5a3a4fded6ac
Shlynz <75536038+shlynz@users.noreply.github.com>
2022-11-11 19:19:11 +0100

    Overloaded pseudorandom#next with a max parameter

--- pseudorandom.js
+++ pseudorandom.js
@@ -2,10 +2,14 @@ const Rng = function(seed){
     this.seed = seed;
 }
 
-Rng.prototype.next = function(){
+Rng.prototype.next = function(max){
     this.seed++;
     const temp = this.seed * 15485863;
-    return (temp * temp * temp % 2038074743) / 2038074743;
+    const randomNumber = (temp * temp * temp % 2038074743) / 2038074743;
+    if(max != undefined || max != null){
+        return Math.floor(randomNumber * (max + 1));
+    }
+    return randomNumber;
 }
 
 module.exports = Rng;
